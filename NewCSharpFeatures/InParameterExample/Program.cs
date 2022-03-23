3b7cb398c2bf178acb8f7970d4788eb467cc9077
Jozsef Kurta <jockaaa@yahoo.com>
2022-03-23 22:07:46 +0200

    Add example for in and params parameter modifiers

---
+++ NewCSharpFeatures/InParameterExample/Program.cs
@@ -0,0 +1,13 @@
+﻿//main
+var foo	= new Foo();
+foo.DoStuff(10);
+
+public class Foo
+{
+	public void DoStuff(in int number)
+	{
+		// the code below will not compile because number is readonly in this context
+		//number++;
+		Console.WriteLine(number);
+	}
+}
---
+++ NewCSharpFeatures/ParamsExample/Program.cs
@@ -0,0 +1,14 @@
+﻿//main
+var foo = new Foo();
+foo.DoStuff(1,2,3,4,5,6);
+
+public class Foo
+{
+	public void DoStuff(params int[] numbers)
+	{
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			Console.WriteLine(numbers[i]);
+		}
+	}
+}
