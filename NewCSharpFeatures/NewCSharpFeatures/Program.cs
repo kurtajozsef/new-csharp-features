8d418c092a275c4768a355b99bbf90a6b8192c7b
Jozsef Kurta <jockaaa@yahoo.com>
2022-03-23 22:00:00 +0200

    Add example for return ref

---
+++ NewCSharpFeatures/NewCSharpFeatures/Program.cs
@@ -0,0 +1,16 @@
+﻿//main
+var foo = new Foo() { num = 1 };
+Console.WriteLine(foo.num);
+ref int foo2 = ref foo.GetFoo();
+// changing foo2 changes foo.num as its reference is taken from GetFoo()
+foo2 = 5;
+Console.WriteLine(foo.num);
+
+public class Foo
+{
+	public int num;
+	public ref int GetFoo()
+	{
+		return ref num;
+	}
+}
